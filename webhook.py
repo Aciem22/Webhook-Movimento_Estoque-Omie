c25a48ffbcbe35b0ad45c8548963b4a536d8e1da
Aciem22 <gustavoaciem@gmail.com>
2025-11-26 10:42:00 -0300

    Adicionando integração com AppsScript

--- webhook.py
+++ webhook.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Request
+import httpx
 
 app = FastAPI()
+
+GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxlUDRnfbeey_yQ75SjsXP0pkA1rx56QeHmucA0i4_Jef3Hheh-3HpJzb-vXb28EV9K/exec"
 
 @app.post("/webhook")
 async def receber_webhook(request: Request):
@@ -8,5 +11,20 @@ async def receber_webhook(request: Request):
     
     print("📦 Webhook recebido:")
     print(body)  # Vai aparecer nos logs do Render
+
+
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                GOOGLE_SCRIPT_URL,
+                json=body,
+                timeout = 10
+            )
+
+        print("📤 Resposta do Google Script:")
+        print(response.text)
+    
+    except Exception as e:
+        print("❌ Erro ao enviar para o Google Script:", e)
 
     return {"status": "ok"}
